ca33cac2ca6b9225a006e090c21f8edbd2dd05a4
Silver4567J <silgold54@gmail.com>
2025-01-15 18:36:02 -0500

    Projecto final Parte 1

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -2,31 +2,28 @@ import React from 'react';
 import './AboutUs.css';
 
 function AboutUs() {
-  return (
-    <div className="about-us-container">
-      {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
-      <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
-      </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-      <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
-      </p>
-      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-
-      <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
-      </p>
-    </div>
-  );
+    return (
+        <div className="about-us-container">
+            {/* <h1 className="about-us-heading">About Us</h1> */}
+            <p className="about-us-description">¡Bienvenido a Paradise Nursery, donde el verde se combina con la serenidad!</p>
+            <p className="about-us-content">
+                En Paradise Nursery, nos apasiona acercarte la naturaleza. Nuestra misión es proporcionar una amplia gama de
+                plantas de alta calidad que no solo realcen la belleza de tu entorno, sino que también contribuyan a un estilo de vida más saludable y
+                sostenible. Desde plantas que purifican el aire hasta plantas aromáticas y fragantes, tenemos algo para todos los amantes de las plantas.
+            </p>
+            {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+            <p className="about-us-content">
+                Nuestro equipo de expertos se dedica a garantizar que cada planta cumpla con nuestros estrictos estándares de calidad y cuidado.
+                Ya sea que sea un jardinero experimentado o que recién esté comenzando su camino hacia la ecología, estamos aquí para ayudarlo en cada paso
+                del camino. No dude en explorar nuestra colección, hacer preguntas y permítanos ayudarlo a encontrar la planta perfecta para su
+                hogar u oficina.
+            </p>
+            {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+            <p className="about-us-content">
+                Únase a nuestra misión de crear un mundo más verde y saludable. Visite Paradise Nursery hoy y experimente la belleza de la naturaleza a su puerta.
+            </p>
+        </div>
+    );
 }
 
 export default AboutUs;
